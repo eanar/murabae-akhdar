b5671d2aa0dbad6ed36d2224e28197fd25bbbc70
eanar <eanar27l7@gmail.com>
2018-09-27 17:28:27 -0700

    Add more present participle rules

--- src/run.py
+++ src/run.py
@@ -23,30 +23,47 @@ class common(object):
                 return callback2(word)
             else:
                 return word
-     
+              
+    def getRandomFileLineTest(dataFile, listType, callback=None, callback2=None):
+        with open(dataFile, 'r') as data:
+            for i, each in enumerate(data):
+                word = callback(each).lower()
+                print word
+                if callback2:
+                    print '  '+callback2(word)
+                else:
+                    print '  '+word
+
     def callback_adjListExq(text):
         return text.split(':')[0]
     
     def callback_verbList(text): return text.strip()
 
     def getVowels(): return 'aeiou'
-
     def getConsonants(): return 'bcdfghjklmnpqrstvwxyz'
-    def getSilentEWords(): return ['close','move','live','have']
+    def getEWordsSilent(): return ['close','move','live','have']
+    def getEWordsNonSilent(): return ['be','see']
 
-    def callback_makePresentParticiple(text, consonants=getConsonants(), vowels=getVowels()):
-        if text.endswith('e'):
-            return text[:-1]+'ing'
-        elif (len(text) == 3) and (text[0] in consonants) and (text[1] in vowels) and (text[2] in consonants):        
-            return text+text[2]+'ing'
-        elif text[-1] == 'l':
-            return text+'ling'
-        elif text in getSilentEWords():
-            return text[:-1]+'ing'
+    def callback_makePresentParticiple(text, consonants=getConsonants(), vowels=getVowels(), eWordsSilent=getEWordsSilent(), eWordsNonSilent=getEWordsNonSilent()):
+        if text in eWordsNonSilent:
+            return text+'ing'
         elif text[-2:] == 'ie':
             return text[:-2]+'ying'
+        elif text in eWordsSilent:
+            return text[:-1]+'ing'
+        elif text[-1] == 'l':
+            return text+'ling'
+        elif text[-2:] == 'ic':
+            return text+'king'
+        elif (len([letter for letter in text if letter in vowels]) >= 2) and (text[-3] in consonants) and (text[-2] in vowels) and (text[-1] in consonants):
+            return text+text[-1]+'ing'
+        elif (len(text) == 3) and (len([letter for letter in text if letter in vowels]) == 1) and (text[-3] in consonants) and (text[-2] in vowels) and (text[-1] in consonants):        
+            return text+text[2]+'ing'
+        elif text.endswith('e'):
+            return text[:-1]+'ing'
         else:
             return text+'ing'
     
     #print getRandomFileLine('../res/exquisite-adjectives', adjListExq, callback_adjListExq)
-    print getRandomFileLine('../res/verbs-list-3', verbList, callback_verbList, callback_makePresentParticiple)
+    #print getRandomFileLine('../res/verbs-list-test', verbList, callback_verbList, callback_makePresentParticiple)
+    print getRandomFileLineTest('../res/verbs-list-test', verbList, callback_verbList, callback_makePresentParticiple)
